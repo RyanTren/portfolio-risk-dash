b5baae1c9459e452f2679b426c448ef685cb743d
ryan <concepting@protonmail.com>
2025-11-16 18:17:53 -0500

    [fix] backend api routing defaults to swagger now,

--- backend/backendAPI/Program.cs
+++ backend/backendAPI/Program.cs
@@ -5,13 +5,16 @@ using backend.backendAPI.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// adds services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// database stuff
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=data.db"));
 
+// DI
 builder.Services.AddScoped<IPortfolioService, PortfolioService>();
 builder.Services.AddScoped<RiskCalculationService>();
 
@@ -22,21 +25,27 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty; // this makes swagger the root url 
+    });
+}
+else
+{
+    app.MapGet("/", () => Results.Json(new { status = "Backend API is running! \n Go to http://localhost:5233/swagger" }));
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-// this is the default routing when you do "dotnet run"
-app.MapGet("/", () => "Backend API is currently running!\n go to http://localhost:5233/swagger");
-
-
+//  applies migrations automatically in dev
 using(var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.EnsureCreated(); 
+    db.Database.Migrate(); //aplied any pending migrations
 }
 
 app.Run();
